9f81645f4324b81cbe1dd4583cab45cc474c7adc
Mohamed Aslam M <mohamedaslamcodes@gmail.com>
2025-06-19 15:03:56 +0530

    Update

--- stepfunction.tf
+++ stepfunction.tf
@@ -1,14 +1,14 @@
 
 # TODO:
 # - If there's only one fleet, the bulk state machines fails as it expects an arrya, but gets a string  
-# - When creating, apply the roles BEFORE creating the step functions
+# - When creating, apply the roles BEFORE creating the step functions as it Results in errors
 
 resource "aws_sfn_state_machine" "bulk_restart_fleet" {
   depends_on = [aws_sfn_state_machine.fleet_restart]
   provider   = aws.us-east-2
   role_arn   = aws_iam_role.bulk_restart_step_function_role.arn
-  name       = "bulk_appstream_fleet_restart_state_machine"
-  definition = templatefile("${path.module}/definitions/bulk-restart-fleet.json", {
+  name       = "fleet_restart_controller_state_machine"
+  definition = templatefile("${path.module}/definitions/fleet-restart-controller.json", {
     fleet_restart_sfn_state_machine_arn = aws_sfn_state_machine.fleet_restart.arn
   })
 }
@@ -16,7 +16,7 @@ resource "aws_sfn_state_machine" "bulk_restart_fleet" {
 resource "aws_sfn_state_machine" "fleet_restart" {
   provider = aws.us-east-2
   role_arn = aws_iam_role.fleet_restart_step_function_role.arn
-  name     = "appstream_fleet_restart_state_machine"
+  name     = "fleet_restart_state_machine"
   definition = templatefile("${path.module}/definitions/fleet-restart.json", {
 
   })
